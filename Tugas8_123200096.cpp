59df406295d53b33163516db73b587afebf07cdb
Ikwnhanif <72723353+Ikwnhanif@users.noreply.github.com>
2020-12-17 12:33:01 +0700

    tugas 8

---
+++ Tugas8_123200096.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+float phi=3.14;
+double Luas(double r);
+double Keliling(double r);
+double Volume(double r, double t);
+int main()
+{
+	char repeat;
+	int pilih;
+	double r,t;
+	do{
+		cout << "Hitung Lingkaran ==========\n";
+		cout << "1. Luas Lingkaran\n";
+		cout << "2. Keliling Lingkaran\n";
+		cout << "3. Volume Tabung\n";
+		cout << "Pilih: "; cin >> pilih;
+		switch(pilih){
+			case 1 :
+				cout << "Luas Lingkaran adalah " << Luas(r) << endl; 
+			break;
+			case 2 :
+				cout << "Keliling Lingkaran adalah " << Keliling(r) << endl; 
+			break;
+			case 3 :
+				cout << "Volume Tabung adalah " << Volume(r,t) << endl; 
+			break;
+			default :
+				cout << "Input Salah :( \n"; 
+		}
+
+		cout << "Ulangi program?(y/n) "; cin >> repeat;
+		cout << endl;
+	}while (repeat=='y');
+
+	cout << "Program selesai, Terima Kasih :)\n";
+	system("pause");
+	return 0;
+}
+
+double Luas(double r){
+	cout << "Jari - Jari	: "; cin >> r; 
+	return phi*r*r;
+}
+
+double Keliling(double r){
+	cout << "Jari - Jari	: "; cin >> r; 
+	return phi*2*r;
+}
+
+double Volume(double r, double t){
+	cout << "Jari - Jari	: "; cin >> r; 
+	cout << "Tinggi		: "; cin >> t; 
+	return phi*r*r*t;
+}
